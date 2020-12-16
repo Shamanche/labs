65fc5fcd8ee83908b484f0640c76f32cc76e1fe7
Shamanche <72706516+Shamanche@users.noreply.github.com>
2020-12-16 18:13:08 +0300

    polynim ready

--- polynom.py
+++ polynom.py
@@ -1,9 +1,30 @@
-def polynom(n, x):
+FILENAME = 'output.txt'
+
+def p(n, x):
     if n == 0:
-        return 1 # P0 = 1
+        return float(1) # P0 = 1
     elif n == 1:
         return x # P1 = x
     else:
-        res = (((2 * n)-1)*x * polynom(n-1, x)-(n-1)*polynom(n-2, x))/float(n)
-        print(res)
-        return res
+        return (((2 * n)-1)*x * p(n-1, x)-(n-1) *  p(n-2, x))/n
+
+try:
+    n = int(input('Введите целое число N: '))
+    x = float(input('Введите вещественное число X:'))
+except ValueError:
+    print ('Ошибка ввода данных')
+    exit(0) # завершение программы
+
+result = []
+for i in range(n + 1):
+    result.append(str(p(i,x)))
+
+with open(FILENAME, 'w') as f:
+    raw = ' '.join(result)
+    f.write(raw)
+    print('Файл {} создан.'.format(FILENAME))
+
+
+
+
+
